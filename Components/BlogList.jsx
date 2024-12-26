d3bae63fa350cc0ca7452523442be1280a70d2c9
muhammedAbdellayassin <ssswar367@gmail.com>
2024-12-26 21:09:11 +0300

    first commit

---
+++ Components/BlogList.jsx
@@ -0,0 +1,81 @@
+import { blog_data } from '@/Assets/assets'
+import React, { useEffect, useState } from 'react'
+import BlogItem from './BlogItem'
+import axios from 'axios'
+
+const BlogList = () => {
+    const [menu,setMenue ]=useState("All")
+    const [blogs, setBlogs]=useState([])
+
+    const fechBllogs=async ()=>{
+      const rsponse = await axios.get('/api/blog')
+      setBlogs(rsponse.data.blogs.reverse())
+      
+    }
+
+    useEffect(()=>{
+        fechBllogs();
+    },[])
+  return (
+    <div>
+      <div className="flex justify-center gap-6 my-10">
+        <button
+          onClick={() => setMenue("All")}
+          className={
+            menu === "All" ? "bg-black text-white py-1 px-4 rounded-sm" : ""
+          }
+        >
+          All
+        </button>
+        <button
+          className={
+            menu === "Technology"
+              ? "bg-black text-white py-1 px-4 rounded-sm"
+              : ""
+          }
+          onClick={() => setMenue("Technology")}
+        >
+          {" "}
+          Technology
+        </button>
+        <button
+          className={
+            menu === "Startup" ? "bg-black text-white py-1 px-4 rounded-sm" : ""
+          }
+          onClick={() => setMenue("Startup")}
+        >
+          {" "}
+          Startup
+        </button>
+        <button
+          className={
+            menu === "Lifestyle"
+              ? "bg-black text-white py-1 px-4 rounded-sm"
+              : ""
+          }
+          onClick={() => setMenue("Lifestyle")}
+        >
+          Lifestyle
+        </button>
+      </div>
+      <div className="flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24">
+        {blogs
+          .filter((item) => (menu === "All" ? true : item.category === menu))
+          .map((item, index) => {
+            return (
+              <BlogItem
+                key={index}
+                id={item._id}
+                image={item.image}
+                title={item.title}
+                description={item.description}
+                category={item.category}
+              />
+            );
+          })}
+      </div>
+    </div>
+  );
+}
+
+export default BlogList
---
+++ app/admin/subscriptions/page.jsx
@@ -0,0 +1,66 @@
+"use client";
+import SubscriptiontableItem from "@/Components/AdminComponent/SubscriptiontableItem";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+const page = () => {
+  const [emails, setEmails] = useState([]);
+
+  const fechEmails = async () => {
+    const response = await axios.get("/api/email");
+    setEmails(response.data.emails)
+  };
+
+  const deleteEmail=async (mongoId) => {
+    const response = await axios.delete("/api/email",{
+    params: { id: mongoId
+
+  }
+    });
+    if(response.data.success){
+      
+      toast.success(response.data.msg)
+      fechEmails()
+    }else{
+      toast.error("Error")
+  
+  }
+}
+  useEffect(()=>{
+    fechEmails()
+  },[])
+
+  return (
+    <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl:16">
+      <h1>All subscription</h1>
+      <div className="relative max-w-[600px] h-[80vh] overflow-x-auto mt-4 border border-gray-400 scollbar-hide">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-xs text-left text-gray-700 uppercase bg-gray-50">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Email subscription
+              </th>
+              <th scope="col" className="px-6 py-3 hidden sm:block">
+                Date
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {emails.map((item,index)=>{
+              return <SubscriptiontableItem key ={index} deleteEmail={deleteEmail} mongoId={item._id} email={item.email} date={item.date}/>
+
+
+            })}
+            <SubscriptiontableItem />
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default page;
