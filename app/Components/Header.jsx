418a3446080e01d78b5e1ff49c692695c03c072e
muhammedAbdellayassin <ssswar367@gmail.com>
2024-12-26 21:34:31 +0300

    heder text

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -40,21 +40,29 @@ const Header = () => {
       <div className="text-center my-8">
         <h1 className="text-3xl sm:text-5xl font-medium">Latest Blogs</h1>
         <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-          ebitis incidunt distinctio! Delectus magnam necessitatibus sunt
-          incidunt sequi tenetur, autem nihil recusandae dolorum!
+          Welcome to Blogger! Discover insights, stories, and
+          inspiration to elevate your everyday life. Whether you're here for
+          fresh ideas, expert tips, or just a spark of creativity, you've found
+          the right place. Dive in and explore a world of content crafted just
+          for you!
         </p>
-        <form onSubmit={onSubmitHandler}
+        <form
+          onSubmit={onSubmitHandler}
           className="flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000] "
           action=""
         >
-          <input onChange={(e)=>setEmail(e.target.value)} value={email}
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             type="email"
             placeholder="Enter your email"
             className="pl-4 outline-none"
             required
           />
-          <button type='submit' className="border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white">
+          <button
+            type="submit"
+            className="border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white"
+          >
             Subscribe
           </button>
         </form>
