a7ff8eacf1e9f8ec72ddfcbb8f2152aad62fea5f
muhammedAbdellayassin <ssswar367@gmail.com>
2024-12-26 21:57:08 +0300

    admin buttons

--- app/Components/AdminComponent/SideBar.jsx
+++ app/Components/AdminComponent/SideBar.jsx
@@ -10,13 +10,13 @@ const SideBar = () => {
         <Image src={assets.logo} width={120} alt="" />
       </div>
       <div className="w-28 sm:w-80 h-[100vh] relative py-12 border border-black">
-        <div className="w-[50%] sm:w-[80%] absolute right-0">
+        <div className="w-[50%] sm:w-[80%] absolute right-3">
           <Link
             href="/admin/addProduct"
             className="flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]"
           >
             <Image src={assets.add_icon} alt="" width={28} />
-            <p>Add blogs</p>
+            <p className="hidden sm:block">Add blogs</p>
           </Link>
 
           <Link
@@ -24,7 +24,7 @@ const SideBar = () => {
             className="mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]"
           >
             <Image src={assets.blog_icon} alt="" width={28} />
-            <p>Blog list</p>
+            <p className="hidden sm:block">Blog list</p>
           </Link>
 
           <Link
@@ -32,12 +32,12 @@ const SideBar = () => {
             className="mt-5 flex items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]"
           >
             <Image src={assets.email_icon} alt="" width={28} />
-            <p>subscription</p>
+            <p className="hidden sm:block">subscription</p>
           </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default SideBar
